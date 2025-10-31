3925b9cb8b7456ec30fe299502d99e297a61b4aa
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2025-10-31 18:27:50 +0900

    feat: #94 ニックネームのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :name, uniqueness: true, length: { in: 2..20 }, allow_blank: true
 end
