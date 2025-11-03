6ac135ca631a17cfa369804d7cd567dec319cfba
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2025-11-03 20:21:51 +0900

    feat: #23 reviewsのアソシエーションを追加

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,6 @@
 class Song < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
   validates :title, presence: true, length: { maximum: 100 }
   validates :composer, length: { maximum: 50 }, allow_blank: true
   validates :arranger, length: { maximum: 50 }, allow_blank: true
