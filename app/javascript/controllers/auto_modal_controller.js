3e353a505d04dfd7c5caf25ca2cd60c2a5d9379d
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2025-11-05 03:33:23 +0900

    feat: #25 モーダル用のコントローラーを作成

---
+++ app/javascript/controllers/auto_modal_controller.js
@@ -0,0 +1,54 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["dialog"]
+
+  static values = {
+    open: { type: Boolean, default: false }
+  }
+
+  connect() {
+    if (!this.openValue) return
+    requestAnimationFrame(() => {
+      this.show()
+      this.removePromptParam()
+    })
+  }
+
+  show() {
+    this.withDialog(dialog => {
+      if (typeof dialog.showModal === "function") {
+        if (!dialog.open) dialog.showModal()
+      } else {
+        dialog.setAttribute("open", "open")
+      }
+    })
+  }
+
+  hide() {
+    this.withDialog(dialog => {
+      if (dialog.open) {
+        dialog.close()
+      } else {
+        dialog.removeAttribute("open")
+      }
+    })
+  }
+
+  withDialog(callback) {
+    const dialog = this.hasDialogTarget ? this.dialogTarget : this.element
+    if (!dialog) return
+    callback(dialog)
+  }
+
+  removePromptParam() {
+    if (!this.openValue) return
+    if (!("replaceState" in window.history)) return
+
+    const url = new URL(window.location.href)
+    if (!url.searchParams.has("review_prompt_song_id")) return
+
+    url.searchParams.delete("review_prompt_song_id")
+    window.history.replaceState(window.history.state, document.title, url)
+  }
+}
