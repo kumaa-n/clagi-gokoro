2d3395ef6e2930281e94864182496234274e05e0
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2025-11-07 02:54:08 +0900

    refactor #124 scopeを用いてSongモデルに処理を集約化

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,19 +2,7 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def top
-    @most_reviewed_songs = Song
-      .left_outer_joins(:reviews)
-      .select("songs.*, COUNT(reviews.id) AS reviews_count, AVG(reviews.overall_rating) AS average_overall_rating")
-      .group(:id)
-      .having("COUNT(reviews.id) > 0")
-      .order(Arel.sql("COUNT(reviews.id) DESC, songs.created_at DESC"))
-      .limit(4)
-
-    @recent_songs = Song
-      .left_outer_joins(:reviews)
-      .select("songs.*, COUNT(reviews.id) AS reviews_count, AVG(reviews.overall_rating) AS average_overall_rating")
-      .group(:id)
-      .order(Arel.sql("songs.created_at DESC"))
-      .limit(4)
+    @most_reviewed_songs = Song.most_reviewed(4)
+    @recent_songs = Song.recent_with_stats(4)
   end
 end
