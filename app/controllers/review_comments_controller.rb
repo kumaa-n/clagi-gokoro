426750ebaf82f3d762ad43c20070ca90828ecf14
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2025-11-06 20:59:04 +0900

    feat: #31 登録済みコメントを取得

--- app/controllers/review_comments_controller.rb
+++ app/controllers/review_comments_controller.rb
@@ -1,6 +1,7 @@
 class ReviewCommentsController < ApplicationController
   before_action :set_song
   before_action :set_review
+  before_action :set_review_comment, only: %i[show]
 
   def create
     @review_comment = @review.review_comments.build(review_comment_params)
@@ -26,6 +27,14 @@ class ReviewCommentsController < ApplicationController
     end
   end
 
+  def show
+    if turbo_frame_request?
+      render partial: "review_comments/review_comment", locals: { review_comment: @review_comment }
+    else
+      redirect_to song_review_path(@song, @review)
+    end
+  end
+
   private
 
   def set_song
@@ -34,6 +43,10 @@ class ReviewCommentsController < ApplicationController
 
   def set_review
     @review = @song.reviews.find(params[:review_id])
+  end
+
+  def set_review_comment
+    @review_comment = @review.review_comments.find(params[:id])
   end
 
   def review_comment_params
