6b8fa9f7bbf59c0dbe9b7b6bac573353c70cefb2
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2025-11-22 23:47:32 +0900

    fix: #166 デプロイ時にエラーとなったため記載を変更

--- db/migrate/20251122080029_add_song_uuid_to_reviews.rb
+++ db/migrate/20251122080029_add_song_uuid_to_reviews.rb
@@ -1,21 +1,15 @@
 class AddSongUuidToReviews < ActiveRecord::Migration[7.2]
-  def up
+  def change
     add_column :reviews, :song_uuid, :uuid
     add_index :reviews, :song_uuid
 
     # 既存データの移行
-    execute <<~SQL
-      UPDATE reviews
-      SET song_uuid = songs.uuid
-      FROM songs
-      WHERE reviews.song_id = songs.id;
-    SQL
+    Review.reset_column_information
+    Review.find_each do |review|
+      review.update_column(:song_uuid, Song.find(review.song_id).uuid)
+    end
 
     # null制約を追加
     change_column_null :reviews, :song_uuid, false
-  end
-
-  def down
-    remove_column :reviews, :song_uuid
   end
 end
