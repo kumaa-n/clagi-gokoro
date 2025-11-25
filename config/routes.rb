02677a8ae806fc1867bc799aeb4a0d388b8c9577
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2025-11-25 18:18:03 +0900

    feat: #156 deviseのコントローラーのひな型を作成し、認証メール無しでログインできるよう変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, only: %i[sessions registrations]
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
 
   root "static_pages#top"
 
