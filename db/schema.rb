a555976def3d084d5b35b63211117ee8595415b6
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2025-11-26 04:50:53 +0900

    feat: #164 Googleログイン用のカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_24_170000) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_25_173433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,10 +56,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_24_170000) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "provider"
+    t.string "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true, where: "((email IS NOT NULL) AND ((email)::text <> ''::text))"
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "reviews", "songs", column: "song_uuid", primary_key: "uuid"
