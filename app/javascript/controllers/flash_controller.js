f5e8c724cfaea190af5d9dd806747ceb4263485c
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2025-12-06 02:15:44 +0900

    fix: #65 フラッシュメッセージのレイアウトを修正

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,11 +1,42 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = {
+    // 消える処理を開始するまでの時間
+    delay: { type: Number, default: 3000 },
+
+    // フェードアウトする時間
+    fadeDuration: { type: Number, default: 1000 }
+  }
+
   connect() {
-    setTimeout(() => {
-      this.element.style.transition = "opacity 1.0s";
-      this.element.style.opacity = "0";
-      setTimeout(() => this.element.remove(), 1000);
-    }, 3000);
+    this.hideTimeout = setTimeout(() => {
+      this.hide()
+    }, this.delayValue)
+  }
+
+  disconnect() {
+    // タイマーをクリーンアップ
+    if (this.hideTimeout) {
+      clearTimeout(this.hideTimeout)
+    }
+    if (this.removeTimeout) {
+      clearTimeout(this.removeTimeout)
+    }
+    // 要素が残っている場合は削除
+    if (this.element && this.element.parentNode) {
+      this.element.remove()
+    }
+  }
+
+  hide() {
+    // フェードアウトアニメーションを開始
+    this.element.style.transition = `opacity ${this.fadeDurationValue}ms`;
+    this.element.style.opacity = "0";
+
+    // フェードアウトアニメーション終了後削除
+    this.removeTimeout = setTimeout(() => {
+      this.element.remove()
+    }, this.fadeDurationValue)
   }
 }
