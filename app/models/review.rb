c431cc2d2472a870116405b73ff90485e21fcbab
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-01-12 17:57:47 +0900

    feat: #44 タグのバリデーションを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -19,6 +19,7 @@ class Review < ApplicationRecord
   validates *RATING_ATTRIBUTES, inclusion: { in: 1..5 }
   validates :song_uuid, uniqueness: { scope: :user_id }
   validates :summary, content_length: { maximum: SUMMARY_MAX_LENGTH }, allow_blank: true
+  validate :validate_tags
 
   before_save :calc_overall_rating
 
@@ -42,6 +43,17 @@ class Review < ApplicationRecord
   end
 
   private
+
+  def validate_tags
+    if tags.size > ReviewTags::MAX_TAGS_PER_REVIEW
+      errors.add(:tags, "は#{ReviewTags::MAX_TAGS_PER_REVIEW}個まで選択できます")
+    end
+
+    invalid = tags - ReviewTags::AVAILABLE_TAGS
+    if invalid.any?
+      errors.add(:tags, "に不正な値が含まれています: #{invalid.join(', ')}")
+    end
+  end
 
   def calc_overall_rating
     # 全ての評価が存在する場合のみ計算
